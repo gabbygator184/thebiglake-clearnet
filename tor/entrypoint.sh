c3d45078285423daa142afee4b08bc356672ab93
gabbygator184 <gabbygator184@proton.me>
2025-06-20 14:24:24 -0300

    Adding LNbits config

--- tor/entrypoint.sh
+++ tor/entrypoint.sh
@@ -12,4 +12,5 @@ chown -R alice:alice /home/alice
 
 sudo -u tor /usr/bin/tor &
 socat tcp4-LISTEN:802,reuseaddr,fork,keepalive SOCKS4A:127.0.0.1:4t4jxmivv6uqej6xzx2jx3fxh75gtt65v3szjoqmc4ugdlhipzdat6yd.onion:80,socksport=9050 &
-socat tcp4-LISTEN:801,reuseaddr,fork,keepalive SOCKS4A:127.0.0.1:ghbtv7lhoyhomyir4xvxaeyqgx4ylxksia343jaat3njqqlkqpdjqcyd.onion:80,socksport=9050
+socat tcp4-LISTEN:801,reuseaddr,fork,keepalive SOCKS4A:127.0.0.1:ghbtv7lhoyhomyir4xvxaeyqgx4ylxksia343jaat3njqqlkqpdjqcyd.onion:80,socksport=9050 &
+socat tcp4-LISTEN:901,reuseaddr,fork,keepalive SOCKS4A:127.0.0.1:6bh7ldsqwlwvdlq5m6vst6ysgjek5jyns2f4johtodzwmkurcqm4jaqd.onion:80,socksport=9050
